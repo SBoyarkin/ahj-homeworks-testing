2ad96c68aab7ca2010685a70447bda0306831057
sboyarkin <sergey.boyarkin@mail.ru>
2025-05-23 14:12:05 +0500

    Добавлен тест isValidCardNumber

--- src/js/app.test.js
+++ src/js/app.test.js
@@ -1,11 +1,40 @@
-import demo from "./app";
+import { Widget } from "./widget"; // Путь к вашему файлу с классом Widget
 
-describe("Пример теста", () => {
-  test.each([
-    { str: "Hello!", expected: "Demo: Hello!" },
-    { str: "", expected: "Demo: " },
-    { str: 100, expected: "Demo: 100" },
-  ])("demo($str)", ({ str, expected }) => {
-    expect(demo(str)).toBe(expected);
+describe("Widget", () => {
+  let widget;
+
+  beforeEach(() => {
+    widget = new Widget({});
+  });
+
+  describe("isValidCardNumber", () => {
+    it("Валидная карта", () => {
+      const validCards = [
+        "4111 1111 1111 1111", // Visa
+        "5555 5555 5555 4444", // Mastercard
+        "2200 0000 0000 0004", // Mir
+      ];
+
+      validCards.forEach((card) => {
+        expect(widget.isValidCardNumber(card)).toBe(true);
+      });
+    });
+
+    it("Не валидная карта", () => {
+      const invalidCards = [
+        "4111 1111 1111 1112",
+        "5555 5555 5555 5555",
+        "1234 5678 9012 3456",
+        "123",
+        "1234 5678 9012 3456 7890",
+        "abcdef ghijkl mnopqr",
+      ];
+
+      invalidCards.forEach((card) => {
+        expect(widget.isValidCardNumber(card)).toBe(false);
+      });
+    });
   });
 });
+
+
